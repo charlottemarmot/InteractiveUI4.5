f0c58640ad30135d43f760af2e59efbe1490cf0c
Charlotte <caw680@gmail.com>
2025-08-06 15:13:03 -0400

    Added @State, created modifications

--- InteractiveUI4.5/ContentView.swift
+++ InteractiveUI4.5/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var name = ""
     @State private var textTitle = "What is your name?"
+    @State private var camp = ""
+    @State private var textTitle2 = "What camp are you in?"
+    @State private var alert = ""
     var body: some View {
         VStack {
             Text(textTitle)
@@ -18,6 +21,13 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray,width:1)
+            Text(textTitle2)
+                .font(.title)
+            TextField("Type the camp you are in...", text: $camp)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray,width:1)
+            Text("Thank you for submitting!")
             Button("Submit Name") {
                 textTitle = "Welcome, \(name)!"
                 name = ""
